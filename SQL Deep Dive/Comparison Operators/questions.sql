22f2eb0537f920b803d1b995ad5421fae0b081ff
vishwasgupta704 <80181752+vishwasgupta704@users.noreply.github.com>
2021-12-31 14:11:29 +0530

    Update questions.sql

--- SQL Deep Dive/Comparison Operators/questions.sql
+++ SQL Deep Dive/Comparison Operators/questions.sql
@@ -2,20 +2,25 @@
 /*
 * Write your query here
 */
-
+SELECT count(*) FROM customers
+where state = 'OR' and gender = 'F';
 -- Who over the age of 44 has an income of 100 000 or more? (excluding 44)
 /*
 * Write your query here
 */
-
+ELECT firstname , lastname FROM customers
+where  age > '44' and income > '100000';
 -- Who between the ages of 30 and 50 has an income less than 50 000?
 -- (include 30 and 50 in the results)
 
 /*
 * Write your query here
 */
-
+SELECT firstname , lastname FROM customers
+where  age >= '30' and age <= '50' and income < '500000';
 -- What is the average income between the ages of 20 and 50? (Excluding 20 and 50)
 /*
 * Write your query here
 */
+SELECT avg(income) FROM customers
+where  age > '20' and age < '50';
